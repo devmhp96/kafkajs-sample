ceea874d831fb20293c8e4b3a36f4bb59f6a0e49
Masoud <masoud@hyvemobile.co.za>
2023-02-22 21:55:32 +0330

    complete

--- src/configuration.js
+++ src/configuration.js
@@ -1,33 +1,39 @@
-const dotenv = require('dotenv');
-const { Kafka, logLevel } = require('kafkajs')
+const dotenv = require("dotenv");
+const { Kafka, logLevel } = require("kafkajs");
 
 const configuration = (() => {
-    dotenv.config();
-    const kafka = new Kafka({
-        logLevel: logLevel.INFO,
-        brokers: [`${process.env.KAFKA_HOST}:${process.env.KAFKA_PORT}`],
-      })
-      
-      const producer = kafka.producer();
+  dotenv.config();
+  const kafka = new Kafka({
+    logLevel: logLevel.NOTHING,
+    brokers: [`${process.env.KAFKA_HOST}:${process.env.KAFKA_PORT}`],
+    // ssl: true,
+    // sasl: {
+    //   mechanism: "scram-sha-512", // scram-sha-256 or plain
+    //   username: process.env.KAFKA_USERNAME,
+    //   password: process.env.KAFKA_PASSWORD,
+    // },
+  });
 
-      const consumerA = kafka.consumer({
-        groupId: "consumerA"
-      });
+  const producer = kafka.producer();
 
-      const consumerB = kafka.consumer({
-        groupId: "consumerB"
-      });
-      
-      const consumerG = kafka.consumer({
-        groupId: "consumerGeneral"
-      })
+  const consumerA = kafka.consumer({
+    groupId: "consumerA",
+  });
 
-      return {
-        producer,
-        consumerA,
-        consumerB,
-        consumerG,
-      }
+  const consumerB = kafka.consumer({
+    groupId: "consumerB",
+  });
+
+  const consumerG = kafka.consumer({
+    groupId: "consumerGeneral",
+  });
+
+  return {
+    producer,
+    consumerA,
+    consumerB,
+    consumerG,
+  };
 })();
 
 module.exports = configuration;
